d1ff6eedc9b134efa256e48638e209cf24afc5bc
Brandon Rodriguez <brodrigu@mines.edu>
2014-04-19 12:47:47 -0600

    Updated to use Requests module for posting Fio and Pi data to ACMx server

--- update.py
+++ update.py
@@ -3,58 +3,76 @@
 import urllib2
 import urllib
 import hashlib
+import requests
 
-# Temperature and pointWindow default to None so legacy code works
 
-# Posts the data from the fio magnetometer sensors to the acmx server
+# This method will post data from the Fio magnetometer to the ACMx server.
+# It uses the 'requests' module for Multipart-Encoding of the image file. See 
+# http://docs.python-requests.org/ for documentation. Prints server response
+# to the console.
+# 
+# Args:
+#		sensorID: The ID of the sensor who recorded the image.
+#		carcount: The reported count of vehicles from the Fio.
+#		battery: The battery level of the Fio (float).
+#		temperature: set to None for legacy compatibility.
+#		window: set to None for legacy compatibility.
+#
+#	Returns:
+#		(nothing)
+# 
 def postFioData(sensorID, carcount,  battery, temperature=None, window=None):
-	dataWindow = ""
 	
-	# Create data array
-	data = [("username", "wsn"), ("password", "raspberryp1"), ("id", sensorID), ("carcount", carcount), ("voltage", battery), ("temperature", temperature), ("window", window)]
+	url = "http://acmxlabs.org/smartlots/fiodata" 
 	
-	# Encode the data
-	encodedData = urllib.urlencode(data)
-
-	# HTTP url
-	path = "http://acmxlabs.org/smartlots/fiodata" 
-
-	# Create request
-	request = urllib2.Request(path, encodedData)
-
-	# Add headers
-	request.add_header("Content-type", "application/x-www-form-urlencoded") 
+	# Construct normal form variables payload.
+	dPayload = {
+			"username": "wsn", 
+			"password": "raspberryp1", 
+			"id": sensorID,
+			"carcount": carcount, 
+			"voltage": battery, 
+			"temperature": temperature,
+			"window": window
+	}
 	
-	# Send request
-	page = urllib2.urlopen(request)
+	# Create a requests object to handle all the urllib2 stuff.
+	r = requests.post(url, data=dPayload)
+	
+	# Print out the server's response.
+	print r.text
 
-	print page.read()
 
-	return page 
-
-
-# Posts the data from the image acquisition Pis to the acmx server
+# This method will post an image to the ACMx server. It uses the 'requests'
+# module for Multipart-Encoding of the image file. See 
+# http://docs.python-requests.org/ for documentation. Prints server response
+# to the console.
+# 
+# Args:
+#		sensorID: The ID of the sensor who recorded the image.
+#		image: A file handle that was opened for binary reading.
+#					e.g. f = open('img.png', 'rb')
+#
+#	Returns:
+#		(nothing)
+# 
 def postPiData(sensorID, image):
-
-	# Create data array
-	data = [("username", "wsn"), ("password", "raspberryp1"), ("id", sensorID)]
-
-	# Encode the data
-	encodedData = urllib.urlencode(data)
-
-	# HTTP url
-	path = "http://acmxlabs.org/smartlots/pidata"
-
-	# Create request
-	request = urllib2.Request(path, encodedData)
-
-	# Add headers
-	request.add_header("Content-type", "application/x-www-form-urlencoded")
-
-	# Send request
-	page = urllib2.urlopen(request)
-
-	print page.read()
-
-	return page
+	
+	url = "http://acmxlabs.org/smartlots/pidata"
+	
+	# Construct the file payload.
+	fPayload = {"image": image}
+	
+	# Construct normal form variables payload.
+	dPayload = {
+			"username": "wsn", 
+			"password": "raspberryp1", 
+			"id": sensorID
+	}
+	
+	# Create a requests object to handle all the urllib2 stuff.
+	r = requests.post(url, data=dPayload, files=fPayload)
+	
+	# Print out the server's response.
+	print r.text
 
